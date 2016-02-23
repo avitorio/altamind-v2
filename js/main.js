d789bc94385ca218335b2222767bdd8e3d20bb1b
Andre Vitorio <andre@vitorio.net>
2016-02-23 22:00:17 +0000

    Our work background set

--- js/main.js
+++ js/main.js
@@ -10,6 +10,27 @@ $( document ).ready(function() {
     }
  });
 
+// Show navbar only after the first Section
+(function ($) {
+  $(document).ready(function(){
+    
+  // hide .navbar first
+  $(".navbar").hide();
+  
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+      if ($(this).scrollTop() > 200) {
+        $('.navbar').fadeIn();
+      } else {
+        $('.navbar').fadeOut();
+      }
+    });
+  });
+});
+  }(jQuery));
+
 // Hero text effect
 jQuery(document).ready(function(){
    $('#hero').mousemove(function(e){
