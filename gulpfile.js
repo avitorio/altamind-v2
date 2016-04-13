fb8af370c7f0cb48b433ce766d64b6f53df633a9
Andre Vitorio <andre@Andres-MacBook-Pro.local>
2016-04-13 23:06:26 +0100

    Big batch of changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
   gulp.src('./scss/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(minifyCss({compatibility: 'ie9'}))
+    .pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
     .pipe(gulp.dest('./css/'))
+
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
 });
 
 // Watch task
